c030324c5d70ef7de2a663d211de932e8b053754
megha1302 <megha.sonkusare@anchanto.com>
2020-01-13 11:28:52 +0530

    adding search to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,13 @@
 class Product < ApplicationRecord
     has_many :orders , through: :order_product
     has_many :order_products
+
+    def self.search(search)
+        if search
+            where(["name LIKE?","%#{search}%"])
+        else
+            all
+        end
+    end 
+          
 end
